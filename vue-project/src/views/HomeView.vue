f0b1fa9ad3db89106afdf3e285ada9cde83c4bb7
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-02-12 13:12:27 -0500

    commit

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+const peopleForHire = [
+  {
+    name: "Yan",
+    codingProwess: 9,
+    price: 25,
+  },
+  {
+    name: "Gabe",
+    codingProwess: 7,
+    price: 20, 
+  },
+  {
+    name: "Noah A.",
+    codingProwess: 7.5,
+    price: 22, 
+  },
+  {
+    name: "Tristian",
+    codingProwess: 4,
+    price: 4,
+  },
+  {
+    name: "Noah R.",
+    codingProwess: 9,
+    price: 25, 
+  },
+]
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
