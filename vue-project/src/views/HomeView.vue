36cc9a6204740af8ff21f094addb346979f98962
rylan-geykhman <johnsmith102508@gmail.com>
2024-03-14 20:24:50 -0400

    firebase testing

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -30,6 +30,28 @@ console.log(balance.balance)
     components: { SlotMachine, Money }
   }
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaKxxKiw1wPY_n72rJZf8svE5oYJx76uA",
+  authDomain: "fir-testing-123-365f7.firebaseapp.com",
+  projectId: "fir-testing-123-365f7",
+  storageBucket: "fir-testing-123-365f7.appspot.com",
+  messagingSenderId: "221500252480",
+  appId: "1:221500252480:web:7af9af338be26649d01aa5",
+  measurementId: "G-KMWRGGP326"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 </script>
 
 
